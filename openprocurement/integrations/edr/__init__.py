4359992f4d0181fb9d94f15afe99e26a1f5aa4bc
Krokop <chezar1995@gmail.com>
2017-03-03 11:56:12 +0200

    add mock to dependency, add nose in buildout, clear __init__.py, add test for scanner

--- openprocurement/integrations/edr/__init__.py
+++ openprocurement/integrations/edr/__init__.py
@@ -1,11 +1,1 @@
 # -*- coding: utf-8 -*-
-from logging import getLogger
-from pkg_resources import get_distribution
-
-PKG = get_distribution(__package__)
-
-LOGGER = getLogger(PKG.project_name)
-
-
-def includeme(config):
-    LOGGER.info('Init edrapi plugin.')
