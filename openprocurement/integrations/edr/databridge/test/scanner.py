9f4402a4539d294e9af3d80549a856a6a15c1558
Krokop <chezar1995@gmail.com>
2017-03-03 15:01:48 +0200

    add first test, add cases to travis.yml

--- openprocurement/integrations/edr/databridge/test/scanner.py
+++ openprocurement/integrations/edr/databridge/test/scanner.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import uuid
 import unittest
 import datetime
 from gevent import sleep
@@ -6,13 +7,10 @@ from gevent.queue import Queue
 from openprocurement.integrations.edr.databridge.scanner import Scanner
 from mock import patch, MagicMock
 from time import sleep
-
-
-def return_dict():
-    return {'prev_page': '',
-            'next_page': '',
-            'data': [{'status': "active.qualification",
-                      'procurementMethodType': 'aboveThresholdUA'}]}
+from munch import munchify
+from restkit.errors import (
+    Unauthorized, RequestFailed
+)
 
 
 class TestScannerWorker(unittest.TestCase):
@@ -33,18 +31,28 @@ class TestScannerWorker(unittest.TestCase):
         """ Try ... """
         tender_queue = Queue(10)
         client = MagicMock()
-        client.sync_tenders.side_effect = return_dict
-        # client.sync_tenders.side_effect = [RequestFailed(), munchify({
-        #     'session': {'headers': {'User-Agent': 'test.agent'}}
-        # })]
+        client.sync_tenders.side_effect = [
+            RequestFailed(),
+            munchify({'prev_page': {'offset': '123'},
+                      'next_page': {'offset': '1234'},
+                      'data': [{'status': "active.qualification",
+                                "id": uuid.uuid4().hex,
+                                'procurementMethodType': 'aboveThresholdUA'}]}),
+            Unauthorized(),
+            munchify({'prev_page': {'offset': '123'},
+                      'next_page': {'offset': '1234'},
+                      'data': [{'status': "active.qualification",
+                                "id": uuid.uuid4().hex,
+                                'procurementMethodType': 'aboveThresholdUA'}]})]
+
         worker = Scanner.spawn(client, tender_queue)
-        sleep(10)
+        sleep(20)
 
         # Kill worker
         worker.shutdown()
         del worker
 
-        self.assertNotEqual(tender_queue.qsize(), 0)
+        self.assertEqual(tender_queue.qsize(), 2)
 
 
 
